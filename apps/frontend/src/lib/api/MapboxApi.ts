012f0b61587eb5e5b89081487f47302aac4d46ab
Moritz Hein <moritz.hein@live.de>
2025-02-24 19:25:58 +0100

    fix(frontend): remove unused env variable imports

--- apps/frontend/src/lib/api/MapboxApi.ts
+++ apps/frontend/src/lib/api/MapboxApi.ts
@@ -1,26 +1,18 @@
-import {
-	PUBLIC_MAPBOX_TOKEN,
-	PUBLIC_MAPBOX_USERNAME,
-	PUBLIC_MAPBOX_STYLE_ID_DAWN,
-	PUBLIC_MAPBOX_STYLE_ID_DAY,
-	PUBLIC_MAPBOX_STYLE_ID_DUSK,
-	PUBLIC_MAPBOX_STYLE_ID_NIGHT
-} from '$env/static/public';
+import { PUBLIC_MAPBOX_TOKEN } from '$env/static/public';
 import polyline from '@mapbox/polyline';
 
-const styles: Record<MapboxStyle, string> = {
+/* const styles: Record<MapboxStyle, string> = {
 	dawn: PUBLIC_MAPBOX_STYLE_ID_DAWN,
 	day: PUBLIC_MAPBOX_STYLE_ID_DAY,
 	dusk: PUBLIC_MAPBOX_STYLE_ID_DUSK,
 	night: PUBLIC_MAPBOX_STYLE_ID_NIGHT
-};
+};*/
 
 export type MapboxStyle = 'dawn' | 'day' | 'dusk' | 'night';
 
 export class MapboxApi {
 	public static buildMapboxStaticImageUrl({
 		waypoints,
-		style,
 		width,
 		height
 	}: {
@@ -29,8 +21,6 @@ export class MapboxApi {
 		width: number;
 		height: number;
 	}): string {
-		const styleId = styles[style];
-
 		const coords = waypoints.map((w) => [w.lat, w.lng] as [number, number]);
 		const pathPolyline = `path-4+ef7c00-0.7(${polyline.encode(coords)})`;
 		// {name}-{label}+{color}({lon},{lat})
